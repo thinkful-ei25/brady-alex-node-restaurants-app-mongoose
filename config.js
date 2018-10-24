444a0916fe4017b3d456f6a6964e371fc6a3755c
Box <bradycfox@gmail.com>
2018-10-24 12:28:29 -0400

    added new db file

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 'use strict';
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
+  process.env.DATABASE_URL ||
+  'mongodb://admin:towels14@ds141043.mlab.com:41043/node-restaurants-app';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
